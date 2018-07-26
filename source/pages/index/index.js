6e429da242ab0a55ca89e8f63fae340ff9c9fb8e
wawez <tikobaghdasaryan9@gmail.com>
2018-07-26 16:18:58 +0300

    project start

---
+++ source/pages/index/index.js
@@ -0,0 +1,6 @@
+import 'normalize.css';
+import './styles.scss';
+import createMenu from '../../components/menu/menu';
+
+var menu = createMenu(['Главная','Блог'], 'menu');
+document.body.appendChild(menu);
