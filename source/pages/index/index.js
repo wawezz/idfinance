a1ac52495deffb15a315bfe394190bd414e81119
wawez <tikobaghdasaryan9@gmail.com>
2018-07-26 21:35:22 +0300

    header added

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -1,6 +1,2 @@
 import '../../components/base/base';
 import './styles.scss';
-import createMenu from '../../components/menu/menu';
-
-var menu = createMenu(['Главная','Блог'], 'menu');
-document.body.appendChild(menu);
