bf794cb280b91f5f3e3486bd716ece8d045f2476
wawez <bagdasaryan@gamblica.com>
2018-08-07 01:47:18 +0300

    menu dropdown added

--- source/components/base/base.js
+++ source/components/base/base.js
@@ -8,4 +8,12 @@ $(function() {
       var fileName = $(this).val().split('\\').pop();
       $(".fileSetter").html(fileName);
     });
+
+    $(document).on("mouseenter", ".header__menuItem", function(){
+      $(this).children('.header__menuDropdown').slideDown(400);
+    });
+
+    $(document).on("mouseleave", ".header__menuItem", function(){
+      $(this).children('.header__menuDropdown').slideUp(400);
+    });
  });
