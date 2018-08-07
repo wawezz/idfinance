7f4b03c546aef54384dbeeef4aef5043784b9af8
wawez <bagdasaryan@gamblica.com>
2018-08-07 04:10:47 +0300

    header done

--- source/components/base/base.js
+++ source/components/base/base.js
@@ -3,17 +3,36 @@ import './styles.scss';
 import 'normalize.css';
 import $ from 'jquery';
 
-$(function() {
-    $(".fileGetter").change(function (){
-      var fileName = $(this).val().split('\\').pop();
-      $(".fileSetter").html(fileName);
-    });
+$(function () {
+  $(".fileGetter").change(function () {
+    var fileName = $(this).val().split('\\').pop();
+    $(".fileSetter").html(fileName);
+  });
 
-    $(document).on("mouseenter", ".header__menuItem", function(){
-      $(this).children('.header__menuDropdown').slideDown(400);
-    });
+  $(document).on("mouseenter", ".header__menuItem", function () {
+    if(!$('.header__nav').hasClass('burgered') && $(this).children('.header__menuDropdown').length > 0) $(this).children('.header__menuDropdown').toggleClass('collapsed');
+  });
 
-    $(document).on("mouseleave", ".header__menuItem", function(){
-      $(this).children('.header__menuDropdown').slideUp(400);
-    });
- });
+  $(document).on("mouseleave", ".header__menuItem", function () {
+    if(!$('.header__nav').hasClass('burgered') && $(this).children('.header__menuDropdown').length > 0 )  $(this).children('.header__menuDropdown').toggleClass('collapsed');
+  });
+
+  $(document).on('click', '.header__burger', function(){
+    $('.header__nav').toggleClass('collapsed');
+    $('.header__nav').toggleClass('burgered');
+    $('body').toggleClass('noscroll');
+  });
+  $(document).on('click', '.header__close', function(){
+    $('.header__nav').toggleClass('collapsed');
+    $('.header__nav').toggleClass('burgered');
+    $('body').toggleClass('noscroll');
+  });
+
+  $(document).on('click', '.header__nav.burgered .header__menuLink', function(e){
+    var element = $(this).parent('.header__menuItem').children('.header__menuDropdown');
+    if(element.length > 0){
+      e.preventDefault();
+      element.toggleClass('collapsed');
+    }
+  });
+});
